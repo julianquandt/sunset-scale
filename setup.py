4532d544eb8ba2c14f4e7cee8e32032b98866a1e
Julian Quandt <julian_quandt@live.de>
2019-04-09 20:14:43 +0200

    add license

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sunset-scale",
+    version="0.0.1",
+    author="Julian Quandt",
+    author_email="julian_quandt@live.de",
+    description="A circular scale to assess ratings, rating-times, action-initiation-times and mouse-tracks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/julianquandt/sunset_scale",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
