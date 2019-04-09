032ce3208a626cef8dc1a74b1e23352e3d93f39b
Julian Quandt <julian_quandt@live.de>
2019-04-09 20:25:56 +0200

    v0.0.2 package

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/julianquandt/sunset_scale",
+    include_package_data = True,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
